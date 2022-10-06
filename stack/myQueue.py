c518db18a56644bdfbf0ce74f53e4a884ae7ae6f
meseret-ale <84620197+meseret-ale@users.noreply.github.com>
2022-10-06 08:46:26 +0300

    Create myQueue.py

---
+++ stack/myQueue.py
@@ -0,0 +1,19 @@
+class MyQueue:
+    def __init__(self):
+        self.queue = []
+
+    def push(self, x: int) -> None:
+        self.queue.append(x)
+
+    def pop(self) -> int:
+        vlu = self.queue.pop()
+        return vlu
+    def peek(self) -> int:
+        vlu = self.queue[0]
+        return vlu
+    def empty(self) -> bool:
+        if len(self.queue) == 0:
+            return True
+        return False
+    
+    
